ef998e94885ee2d4555425695f21f8a04a4ac8d5
AudeC2103 <aude.campillo@gmail.com>
2024-07-12 15:16:04 +0200

    Chgmt nom Publication 2

--- single-job_held.php
+++ single-job_held.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
-    <h1><?php the_field('job_title'); ?></h1>
-    <p><?php the_field('job_subtitle'); ?></p>
+    <h1><?php the_field('job__title'); ?></h1>
+    <p><?php the_field('job__subtitle'); ?></p>
+    <p><?php the_field('job__start_date'); ?></p>
 
 <?php get_footer(); ?>
